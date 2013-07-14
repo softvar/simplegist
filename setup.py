262abf9808cb0ad7de422c1e8d77fbe1fe4ae2a9
MVar2902 <varun2902@gmail.com>
2013-07-15 02:02:18 +0530

    yay, added tarball for download using tags

--- setup.py
+++ setup.py
@@ -10,16 +10,15 @@ if sys.version_info[:2] < (2,6):
 
     setup(
         name = 'gist',
-        packages = ['gist'], # this must be the same as the name above
+        packages = ['gist'], 
         version = '0.1',
         install_requires=required,
         description = 'Python wrapper for Gist ',
         long_description=open('README.rst').read(),
         author = 'Varun Malhotra',
         author_email = 'varun2902@gmail.com',
-        url = 'https://github.com/softvar/gist',   # use the URL to the github repo
-        download_url = 'https://github.com/softvar/gist/tarball/0.1', # I'll explain this in a second
-        keywords = ['gist', 'github', 'API'], # arbitrary keywords
+        url = 'https://github.com/softvar/gist', 
+        download_url = 'https://github.com/softvar/GistApi-Wrapper-python/tarball/0.1',        keywords = ['gist', 'github', 'API'], # arbitrary keywords
         license = 'MIT', 
         classifiers = [],
     )
