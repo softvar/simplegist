0619e4d566f2ceb2ca4470759772173669f74202
MVar2902 <varun2902@gmail.com>
2013-07-19 02:49:18 +0530

    :sparkles: Release 0.3.2 :sparkles:

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ if sys.version_info[:2] < (2,6):
 setup(
     name = 'simplegist',
     packages = ['simplegist'], 
-    version = '0.3',
+    version = '0.3.2',
     install_requires=required,
     description = 'Python wrapper for Gist ',
     long_description=open('README.rst').read(),
     author = 'Varun Malhotra',
     author_email = 'varun2902@gmail.com',
     url = 'https://github.com/softvar/gist', 
-    download_url = 'https://github.com/softvar/GistApi-Wrapper-python/tarball/0.3',
+    download_url = 'https://github.com/softvar/GistApi-Wrapper-python/tarball/0.3.2',
     keywords = ['gist', 'github', 'API'],
     license = 'MIT', 
     classifiers = (
